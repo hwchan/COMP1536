4efc908157ae52cd59b1722751ddfc81c5649a96
Howie Chan <hw.chan@hotmail.com>
2015-03-16 17:03:40 -0700

    stats update

--- G12/championstats.sql
+++ G12/championstats.sql
@@ -29,10 +29,10 @@ DROP TABLE IF EXISTS `champs`;
 CREATE TABLE `champs` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Name` char(35) NOT NULL DEFAULT '',
-  `Hitpoints` float(5,2) NOT NULL DEFAULT '',
-  `Hpinc` int(2) NOT NULL DEFAULT '',
+  `Hitpoints` float(5,2) NOT NULL DEFAULT '0',
+  `Hpinc` int(2) NOT NULL DEFAULT '0',
   `Mana` float(5,2) NOT NULL DEFAULT '0',
-  `Mpinc` int(2)) NOT NULL DEFAULT '0',
+  `Mpinc` int(2) NOT NULL DEFAULT '0',
   `Attackdmg` float(5,3) NOT NULL DEFAULT '0',
   `ADinc` float(4,3) NOT NULL DEFAULT '0',
   `Attackspeed` float(4,3) NOT NULL DEFAULT '0',
@@ -68,8 +68,8 @@ CREATE TABLE `champs` (
 -- ORDER BY:  `ID`
 
 INSERT INTO `champs` VALUES (1,'Trynadamere',625.64,98,100,0,61.376,3.2,0.67,2.9,24.108,3.1,32.1,1.25,0,0,0,12,0,0,0,14,190,1.2,1,9,0,0,0,90);
-INSERT INTO `champs` VALUES (1,'Amumu',625.64,98,100,0,53.384,4.8,0.638,2.19,23.544,3.3,32.1,1.25,280,0,0.7,9,84,0,0.2,1,175,0,0.5,6,350,0,0.8,110);
-INSERT INTO `champs` VALUES (1,'Annie',511.68,76,334,50,50.41,2.625,0.579,1.36,12.99,4,30,0,220,0,0.8,4,250,0,0.85,8,60,0,0.2,10,425,0,0.8,80);
+INSERT INTO `champs` VALUES (2,'Amumu',625.64,98,100,0,53.384,4.8,0.638,2.19,23.544,3.3,32.1,1.25,280,0,0.7,9,84,0,0.2,1,175,0,0.5,6,350,0,0.8,110);
+INSERT INTO `champs` VALUES (3,'Annie',511.68,76,334,50,50.41,2.625,0.579,1.36,12.99,4,30,0,220,0,0.8,4,250,0,0.85,8,60,0,0.2,10,425,0,0.8,80);
 
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
