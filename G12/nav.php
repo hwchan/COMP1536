ec72640e4ce5b84bb971407328feafcf740a7580
Howie Chan <hw.chan@hotmail.com>
2015-03-16 17:12:24 -0700

    Added php versions of everything

---
+++ G12/nav.php
@@ -0,0 +1,62 @@
+<a href="index.php"><img alt="League of Legends logo" src="images/League_of_legends_logo_transparent.png" width="190" height="75"></a>
+<!-- Nav bar -->
+<nav class="navbar navbar-inverse navbar-fixed-top">
+	<div class="navbar-header">
+		<!-- Collapsed navbar hamburger -->
+		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button>
+		<a class="navbar-brand"></a>
+	</div>
+	<div id="navbar" class="collapse navbar-collapse">
+		<ul class="nav navbar-nav">
+			<?php echo $active == "index" ? '<li class="active">' : '<li>'; ?>
+				<a href="index.php">News</a></li>
+			<?php echo $active == "intro" ? '<li class="active">' : '<li>'; ?>
+				<a href="intro.php">Intro</a></li>
+			<?php echo $active == "videos" || $active == "lcs" ? '<li class="active">' : '<li>'; ?>
+				<!-- Show the dropdown for Media if Javascript is enabled -->
+				<script type="text/javascript">
+					document.write(" \
+					<a href=\"\" data-toggle=\"dropdown\">Media<span class=\"caret\"></span></a> \
+					<ul class=\"dropdown-menu inverse-dropdown\"> \
+						<li><a href=\"videos.php\">Videos</a></li> \
+						<li><a href=\"lcs.php\">Pro teams</a></li> \
+					</ul> \
+					")
+				</script>
+				<!-- Else show plain links -->		
+				<noscript>
+					<div id="noscript-media">
+						<a href="videos.php">Videos</a><br>
+						<a href="lcs.php">Pro teams</a>
+					</div>
+				</noscript>		
+			</li>
+			<?php echo $active == "champions" ? '<li class="active">' : '<li>'; ?>
+				<a href="champions.php">Champions</a></li>
+			<?php echo $active == "items" ? '<li class="active">' : '<li>'; ?>
+				<a href="items.php">Items</a></li>
+			<?php echo $active == "calculator" ? '<li class="active">' : '<li>'; ?>
+				<a href="calculator.php">Calculator</a></li>
+			<li>
+				<!-- Use pop up modal if Javascript is enabled -->
+				<script type="text/javascript">
+					document.write(" \
+					<a href=\"#signin\" data-toggle=\"modal\" data-target=\"#signInModal\">Sign in</a> \
+					")
+				</script>
+				<!-- Else show plain links -->
+				<noscript>
+					<div id="noscript-signin">
+						<a href="signin.html">Sign in</a>
+					</div>
+				</noscript>
+			</li>
+		</ul>
+	</div><!--/.nav-collapse -->
+</nav>
+
